01bf8efbc3b084bf796e437c2d65c4b0803fc0af
unknown <vortifexyt@gmail.com>
2023-09-25 09:42:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table id="mainTable">
+          <tr>
+            <td>a</td>
+            <td>b</td>
+          </tr>
+          <tr>
+            <td>c</td>
+            <td>d</td>
+          </tr>
+          <tr>
+            <td>e</td>
+            <td>f</td>
+          </tr>
+          <tr>
+            <td>g</td>
+            <td>h</td>
+          </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
